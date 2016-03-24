fa97b1567bf59d1997f6a6422b76614404b5e435
yiheyis <yiheyis@gmail.com>
2016-03-24 17:53:45 +0000

    made content

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-alert();
+//Build the html elements
+//create div
+var content=document.createElement("div");
+
+//place content div in the body
+document.body.appendChild(content);
+
+//Put some words inside the content div
+content.innerHTML="Hello World!"
