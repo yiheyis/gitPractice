5478212431c4ae3c83f4d73988ac410c3bd8ac43
yiheyis <yiheyis@gmail.com>
2016-03-24 18:11:21 +0000

    centered content

--- main.js
+++ main.js
@@ -6,4 +6,15 @@ var content=document.createElement("div");
 document.body.appendChild(content);
 
 //Put some words inside the content div
-content.innerHTML="Hello World!"
+content.innerHTML="Hello World!";
+
+//increase text size 
+content.style.fontSize="50px";
+//change background
+content.style.background="teal";
+//half screen width
+content.style.width="50%";
+//center the div
+content.style.textAlign="center";
+//center the content div
+content.style.margin="10% auto";
